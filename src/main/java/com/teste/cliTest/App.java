6cb1fbc4d5cab59820fecae3b90425bbaa9d2180
Vinicius Barbosa <viniciuscicone@gmail.com>
2024-08-31 14:19:49 -0300

    adding first initial files App.java and AppTest.java

--- src/main/java/com/teste/cliTest/App.java
+++ src/main/java/com/teste/cliTest/App.java
@@ -1,2 +1,7 @@
-package com.teste.cliTest;public class App {
+package com.teste.cliTest;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
 }
--- src/test/java/com/teste/cliTest/AppTest.java
+++ src/test/java/com/teste/cliTest/AppTest.java
@@ -1,4 +1,11 @@
+package com.teste.cliTest;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 class AppTest {
-  
+
+    @Test
+    void main() {
+    }
 }
