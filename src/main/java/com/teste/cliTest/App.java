c7bf35f556724ffff4a0f45ade2eabd5436fe15d
Vinicius Barbosa <viniciuscicone@gmail.com>
2024-08-31 15:33:14 -0300

    adding main method in App.java

--- src/main/java/com/teste/cliTest/App.java
+++ src/main/java/com/teste/cliTest/App.java
@@ -1,7 +1,14 @@
 package com.teste.cliTest;
 
 public class App {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        if (args.length < 3) {
+            System.out.println("Uso de CLI comando: java -jar cli.jar analyze --depth <n> \"{phrase}\" [--verbose]");
+            return;
+        }
+        int depth = Integer.parseInt(args[2]);
+        String phrase = args[3];
+        boolean verbose = args.length > 4 && args[4].equals("--verbose");
     }
 }
--- src/test/java/com/teste/cliTest/AppTest.java
+++ src/test/java/com/teste/cliTest/AppTest.java
@@ -3,6 +3,7 @@ package com.teste.cliTest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 class AppTest {
 
     @Test
